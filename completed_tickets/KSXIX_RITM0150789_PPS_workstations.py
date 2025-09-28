6c874c89f07348efcd97c9ab5df2e0950d2a5fb0
James Lemaster <james.lemaster@gainwelltechnologies.com>
2025-09-28 14:53:23 -0400

    Reorginized Repository

---
+++ completed_tickets/KSXIX_RITM0150789_PPS_workstations.py
@@ -0,0 +1,141 @@
+#-------------------------------------------------
+#   About - See Updates and Notes at bottom of script for more information.
+#-------------------------------------------------
+
+'''
+Ticket RITM00150789
+'''
+
+#-------------------------------------------------
+#   Imports
+#-------------------------------------------------
+
+import os, copy
+
+#-------------------------------------------------
+#   Variables
+#-------------------------------------------------
+
+# location of local repository directory:
+source_directory = "C:\\VS_Managed_Accounts\\KSXIX\\PROD"
+
+# where the output files shold go, dont use source location
+output_directory = "C:\\Users\\jlemaster3\\OneDrive - Gainwell Technologies\\Documents\\_Ticket_Repo\\RITM0150789\\Diffrences\\PPS"
+
+output_log_file = os.path.join(output_directory, 'env_change_log.txt')
+
+# Key - Value piar, key string is the source string to saerch for, and the value is what to replace it with.
+find_replace_strings = {
+    "@MACH1#" :"@MACH8#",
+}
+
+#-------------------------------------------------
+#   Functions
+#-------------------------------------------------
+
+# collects all files in source folder path that end with items found in file_types list
+def get_jil_files (source_path:str, file_types:list=['.jil', '.job'])->list[str]:
+    path = os.path.abspath(source_path)
+    if not os.path.exists(path):
+        print (f"Path not found : {source_path}")
+    file_list = []
+    for dir_path, dirs, files in os.walk(path):
+        for file in files:
+            if any([file.lower().endswith(e) for e in file_types]):
+                file_list.append(os.path.join(dir_path,file))
+    return file_list
+
+# loops over each file and removes any file that does not contain at least one of the provided terms in search_terms list, terms must be string values
+def files_with_search_terms (file_path_list:list, search_terms:list):
+    file_holder = []
+    for file in file_path_list:
+        if not os.path.exists(file):
+            print (f"Can not filter file {os.path.basename}, Unable to find file : {file}")
+            continue
+        if os.path.isfile(file):
+            with open(file, 'r') as f:
+                for line in f:
+                    if any(_t in line for _t in search_terms) and (file not in file_holder):
+                        file_holder.append(file)
+    return file_holder
+
+# adds draft to job stream text in source stream
+def add_Draft_to_String (source_string:str):
+    _lines = str(source_string).split('\n')
+    _js_list = []
+    _description_lines = []
+    _has_draft = []
+    for _id, _line in enumerate(_lines):
+        if 'SCHEDULE' in _line:
+            _js_list.append(_id)
+        if ':' in _line[0:2]:
+            _js_list.append(_id)
+        if 'DESCRIPTION' in _line:
+            _description_lines.append(_id)
+        if 'DRAFT' in _line:
+            _has_draft.append(_id)
+    _id_sets = list(zip(_js_list[::2], _js_list[1::2]))
+    for _disc_line in _description_lines:
+        for _set in _id_sets:
+            already_draft = False
+            if (len(_has_draft) >= 1):
+                for _draft_id in _has_draft:
+                    if _set[0] < _draft_id < _set[1] :
+                        already_draft = True
+            if (_set[0] < _disc_line < _set[1]) and (already_draft == False):
+                _lines.insert(int(_disc_line+1), 'DRAFT')
+    string_with_draft = '\n'.join(_lines)
+    return string_with_draft
+
+# output files will be saved in the provided output_path with the same file name as the source
+# output file will contain both the orriginal contents and the duplicated/edited contents in the same file.
+def process_files (path_list:list, seach_replace_dictionary:dict, source_path:str, output_path:str, overwrite_original:bool=False, keep_original:bool=False, original_to_DRAFT:bool=False):
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    for counter, filePath in enumerate(path_list):
+        file_name = os.path.basename(filePath)
+        if overwrite_original == False:
+            _rel_path = os.path.relpath(os.path.dirname(filePath), source_path)
+            _output_path = os.path.join(output_path, _rel_path)
+            if not os.path.exists(_output_path):
+                os.makedirs(_output_path)
+            output_filePath = os.path.join(_output_path, file_name)    
+        else:
+            output_filePath = filePath
+        if os.path.isfile(filePath):
+            source_file = open(filePath, "r")
+            source_string = source_file.read()
+            duplicate_str = str(copy.deepcopy(source_string))
+            for search_key, repalce_val in seach_replace_dictionary.items():
+                duplicate_str = duplicate_str.replace(str(search_key).upper(), str(repalce_val).upper())
+            with open(output_filePath, "w") as output_file:
+                if keep_original == True:
+                    if original_to_DRAFT == True:
+                        output_file.write(add_Draft_to_String(source_string))
+                    else:
+                        output_file.write(source_string)
+                    output_file.write("\n\n")
+                output_file.write(duplicate_str)
+            print (f"{counter+1} - {output_filePath}")
+        
+
+#-------------------------------------------------
+#   Initialize Script
+#-------------------------------------------------
+
+if __name__ == "__main__":
+    # get all jil files in path and all subdirectories
+    jil_files_dictionary = get_jil_files(source_directory)
+    #collects saerchtersm as a list form source dictionary
+    search_terms = list(find_replace_strings.keys())
+    #filter list of files down to those that contian the sarch terms
+    fitlered_files = files_with_search_terms (jil_files_dictionary, search_terms)
+    # create new files that contains a copy of the edited contents
+    process_files(fitlered_files, find_replace_strings, source_directory, output_directory)
+
+#-------------------------------------------------
+#   Updates / Notes
+#-------------------------------------------------
+
+# 2024/08/30 - Scipt created by James Lemaster
+# script is used to create duplicate Job Streams and all Jobs within for workstation migration.
---
+++ completed_tickets/MSXIX_RITM0176143_remove_005_006.py
@@ -0,0 +1,155 @@
+#-------------------------------------------------
+#   About - See Updates and Notes at bottom of script for more information.
+#-------------------------------------------------
+
+'''
+Ticket RITM0176143
+'''
+
+#-------------------------------------------------
+#   Imports
+#-------------------------------------------------
+
+import os, copy
+
+#-------------------------------------------------
+#   Variables
+#-------------------------------------------------
+
+# location of local repository directory:
+source_directory = "C:\\VS_Managed_Accounts\\MSXIX\\PROD"
+
+# where the output files shold go, dont use source location
+output_directory = "C:\\Users\\jlemaster3\\OneDrive - Gainwell Technologies\\Documents\\_Ticket_Repo\\RITM0176143\\removed_005_006"
+
+
+output_log_file = "C:\\Users\\jlemaster3\\OneDrive - Gainwell Technologies\\Documents\\_Ticket_Repo\\RITM0176143\\MSXIX_removeing_old_005_006_ws_log.txt"
+
+search_terms = [
+    "@APP3#",
+    "@APP4#"
+]
+
+# contains output Log strings.
+_output_log_holder = []
+
+
+#-------------------------------------------------
+#   Functions
+#-------------------------------------------------
+
+# collects all files in source folder path that end with items found in file_types list
+def get_jil_files (source_path:str, file_types:list=['.jil', '.job'])->list[str]:
+    path = os.path.abspath(source_path)
+    if not os.path.exists(path):
+        print (f"Path not found : {source_path}")
+    file_list = []
+    for dir_path, dirs, files in os.walk(path):
+        for file in files:
+            if any([file.lower().endswith(e) for e in file_types]):
+                file_list.append(os.path.join(dir_path,file))
+    return file_list
+
+# loops over each file and removes any file that does not contain at least one of the provided terms in search_terms list, terms must be string values
+def files_with_search_terms (file_path_list:list, search_terms:list):
+    file_holder = []
+    for file in file_path_list:
+        if not os.path.exists(file):
+            print (f"Can not filter file {os.path.basename}, Unable to find file : {file}")
+            continue
+        if os.path.isfile(file):
+            with open(file, 'r') as f:
+                for line in f:
+                    if any(_t in line for _t in search_terms) and (file not in file_holder):
+                        file_holder.append(file)
+    return file_holder
+
+
+def get_stream_bounds (source_string:str, search_terms:list[str]):
+    _source_lines = str(source_string).split('\n')    
+    _stream_start_ids:list[int] = []
+    _stream_edge_ids:list[int] = []
+    _stream_end_ids:list[int] = []
+    for _line_id, _line in enumerate(_source_lines):
+        if "SCHEDULE" in str(_line).strip()[0:9]:
+            _stream_start_ids.append(_line_id)
+        if ':' in _line[0:2]:
+            _stream_edge_ids.append(_line_id)
+        if "END" in str(_line).strip()[0:4] and 'ENDJOIN' not in str(_line).strip():
+            _stream_end_ids.append(_line_id)
+    _cleaned_lines = []
+    for _stream_id in range(len(_stream_start_ids)):
+        _stream_idx = _stream_start_ids[_stream_id]
+        _stream_end = _stream_end_ids[_stream_id]
+        _remove = False
+        for _t in search_terms:
+            if _t in _source_lines[_stream_idx]:
+                _output_log_holder.append(f"    - Removing lines betwen [{_stream_idx}] - [{_stream_end}] - Found : '{_t}'")
+                _remove = True
+        if _remove == False:
+            for _i in range(_stream_idx, _stream_end +2):
+                _cleaned_lines.append(_source_lines[_i])
+
+    return '\n'.join(_cleaned_lines)
+
+
+def remove_jobstreams_by_searchTerms (path_list:list,  output_path:str, search_terms:list[str]) :    
+    for counter, filePath in enumerate(path_list):
+        try:
+            file_name = os.path.basename(filePath)
+            if os.path.isfile(filePath):
+                # output_filePath
+                _rel_path = os.path.relpath(filePath, source_directory)
+                _target_path = os.path.join(output_path, os.path.dirname(_rel_path))
+                if not (os.path.exists(_target_path)):
+                    os.makedirs(_target_path)
+                output_filePath = os.path.join(_target_path, file_name)    
+                #read_sourceFile
+                source_file = open(filePath, "r")
+                source_string = source_file.read()
+                _output_log_holder.append(f" [{counter+1}] - checking file {os.path.basename(filePath)}")
+                _cleaned_string = get_stream_bounds( str(copy.deepcopy(source_string)) , search_terms).strip().replace("### REMOVE THIS NOTE ###", "")
+                with open(output_filePath, "w") as output_file:
+                    output_file.write(_cleaned_string)
+                    _output_log_holder.append(f" [{counter+1}] - Created cleaned file: {output_filePath}")
+        except Exception as e:
+            _output_log_holder.append(f" [{counter+1}] - Error processing file : {os.path.basename(filePath)} - {e}")
+        
+        _output_log_holder.append('-'*100)      
+                        
+
+    
+        
+
+#-------------------------------------------------
+#   Initialize Script
+#-------------------------------------------------
+
+if __name__ == "__main__":
+    # get all jil files in path and all subdirectories
+    jil_files_dictionary = get_jil_files(source_directory)
+    #filter list of files down to those that contian the sarch terms
+    fitlered_files = files_with_search_terms (jil_files_dictionary, search_terms)
+    # create new files that contains a copy of the edited contents
+
+    _output_log_holder.append(f"Running Search and Replace process")
+    _output_log_holder.append('-'*100)
+    _output_log_holder.append(f"Source directory : '{source_directory}'")
+    _output_log_holder.append(f"Output directory : '{output_directory}'")
+    _output_log_holder.append('-'*100)
+
+    _output_log_holder.append(f"Number of Search Terms: [{len(search_terms)}]")
+    for _term_count, _term in enumerate(search_terms):
+        _output_log_holder.append(f"  [{_term_count+1}] - {_term}")
+    _output_log_holder.append('-'*100)
+    _output_log_holder.append(f"Number of files found in source Directory: [{len(jil_files_dictionary)}]")
+    _output_log_holder.append(f"Number of files found with 1 or more terms: [{len(fitlered_files)}]")
+    _output_log_holder.append('-'*100)
+
+    remove_jobstreams_by_searchTerms(fitlered_files, output_directory, search_terms)
+
+
+    with open (output_log_file, 'w') as _log_file:
+        _log_file.write("\n".join(_output_log_holder))
+
+    print (f"Done processing")
